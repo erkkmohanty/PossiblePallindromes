085541777d3e9a893c8f2d4838f601f0014824dc
erkkmohanty <er.kkmohanty@gmail.com>
2018-10-07 14:00:02 +0530

    Add project files.

---
+++ PossiblePallindromes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PossiblePallindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
